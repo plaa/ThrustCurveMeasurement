ca5633c4b30d7fe86e07c20129f8761315b17724
Sampo Niskanen <sampo.niskanen@iki.fi>
2013-07-24 20:51:17 +0300

    Write backup file while recording

--- src/tcm/gui/MeasurementDialog.java
+++ src/tcm/gui/MeasurementDialog.java
@@ -4,10 +4,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
 import java.io.CharArrayWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -18,6 +23,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 
 import net.miginfocom.swing.MigLayout;
@@ -28,6 +34,7 @@ import tcm.data.DataPoint;
 import tcm.data.MeasurementInstance;
 import tcm.data.MeasurementListener;
 import tcm.data.MeasurementSource;
+import tcm.defaults.Defaults;
 import tcm.document.Measurement;
 import tcm.document.MeasurementDocument;
 import tcm.properties.PropertyList;
@@ -48,16 +55,19 @@ public class MeasurementDialog extends JDialog {
 	private MeasurementInstance measurementInstance;
 	
 	private JToggleButton record;
+	private JTextField backupDirField;
 	private RealtimeGraph graph;
 	private Calibration calibration = new Calibration();
 	private PropertyList properties;
 	
 	private Recorder recorder;
+	private BackupRecorder backupRecorder;
 	
 	private Provider<EditorFrame> editorProvider;
 	
+	
 	@Inject
-	public MeasurementDialog(RealtimeGraph graph, Provider<EditorFrame> editorProvider) {
+	public MeasurementDialog(RealtimeGraph graph, Provider<EditorFrame> editorProvider, Defaults defaults) {
 		super(null, "Measuring...", ModalityType.MODELESS);
 		
 		this.editorProvider = editorProvider;
@@ -102,6 +112,19 @@ public class MeasurementDialog extends JDialog {
 			}
 		});
 		panel.add(record, "growx, wrap para");
+		
+		
+		JLabel label;
+		String tip = "A backup of the recorded data is automatically stored in CSV format in the backup directory.  Leave empty for no backup.";
+		label = new JLabel("Backup directory:");
+		label.setToolTipText(tip);
+		panel.add(label, "spanx, split");
+		
+		backupDirField = new JTextField();
+		backupDirField.setText(defaults.getString("Measurement.backupDir", System.getProperty("java.io.tmpdir", "/tmp")));
+		backupDirField.setEditable(true);
+		backupDirField.setToolTipText(tip);
+		panel.add(backupDirField, "growx, wrap para");
 		
 		
 		split.setLeftComponent(panel);
@@ -173,8 +196,13 @@ public class MeasurementDialog extends JDialog {
 			stopRecord();
 		}
 		
+		
 		recorder = new Recorder();
 		measurementInstance.addListener(recorder);
+		
+		backupRecorder = new BackupRecorder();
+		measurementInstance.addListener(backupRecorder);
+		
 		record.setText(RECORDING_TEXT);
 	}
 	
@@ -184,6 +212,8 @@ public class MeasurementDialog extends JDialog {
 		}
 		
 		measurementInstance.removeListener(recorder);
+		measurementInstance.removeListener(backupRecorder);
+		backupRecorder.close();
 		record.setText(RECORD_TEXT);
 		
 		Measurement m = new Measurement();
@@ -199,6 +229,7 @@ public class MeasurementDialog extends JDialog {
 		frame.setVisible(true);
 		
 		recorder = null;
+		backupRecorder = null;
 	}
 	
 	
@@ -232,6 +263,72 @@ public class MeasurementDialog extends JDialog {
 			dataErrors++;
 		}
 		
+	}
+	
+	
+	private class BackupRecorder implements MeasurementListener {
+		
+		private BufferedWriter writer = null;
+		
+		public BackupRecorder() {
+			File dir = new File(backupDirField.getText());
+			if (!dir.isDirectory()) {
+				System.err.println("WARNING:  '" + dir + "' is not a directory, not making backup recordings.");
+				return;
+			}
+			
+			SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+			String date = fmt.format(new Date());
+			try {
+				File file = File.createTempFile("ThrustCurveMeasurement-" + date + "-", ".csv", dir);
+				System.out.println("Writing backup of measurement to " + file);
+				writer = new BufferedWriter(new FileWriter(file));
+				writer.write("# Backup of measurement started on " + date + "\n");
+			} catch (IOException e) {
+				System.err.println("WARNING:  Unable to open backup file writer, not making backup: " + e.getMessage());
+			}
+		}
+		
+		public void close() {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+				}
+			}
+			writer = null;
+		}
+		
+		@Override
+		public void processData(List<DataPoint> data) {
+			if (writer == null)
+				return;
+			
+			for (DataPoint p : data) {
+				write(p.getTime() + "," + p.getTimestamp() + "," + p.getValue() + "\n");
+			}
+		}
+		
+		@Override
+		public void timingMiss() {
+			write("# Timing miss\n");
+		}
+		
+		@Override
+		public void dataError() {
+			write("# Data error\n");
+		}
+		
+		private void write(String s) {
+			if (writer != null) {
+				try {
+					writer.write(s);
+				} catch (IOException e) {
+					System.err.println("WARNING:  Error writing to backup file: " + e.getMessage());
+					close();
+				}
+			}
+		}
 	}
 	
 }
